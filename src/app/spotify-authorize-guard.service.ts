fca823e6805ce321a127351b8985a1babc0fe8eb
LucasNahuel <munozlucas96@gmail.com>
2023-09-04 23:10:39 -0300

    adds favicon and title

--- src/app/spotify-authorize-guard.service.ts
+++ src/app/spotify-authorize-guard.service.ts
@@ -28,4 +28,6 @@ export class SpotifyAuthorizeGuardService implements CanActivate{
       this.router.navigate(['/login']);
     
   }
+
+
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8888/'
+  apiUrl: 'https://angular-spotify-api.vercel.app/'
 };
 
 /*
